9bc30da85d247eb7daa76e00bda19fef6f3ec9ea
mechael7 <mechaelmandausanduo7@gmail.com>
2026-02-25 21:16:38 +0700

    perubahan di css dan menambah kan composer dan npm

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,20 +8,33 @@
     </div>
     <div class="blog">
         <div class="log1">
-            <img src="{{ asset('image/photo.5.jfif') }}" alt="">
-            <p>category</p>
+            <form action="/form">
+                <div class="log2">
+                    <a href=""><img src="{{ asset('image/photo.5.jfif') }}" alt=""></a>
+                    <button>category</button>
+                </div>
+            </form>
+            <p class="p">12 Febuari 2026</p>
+            <a href=""><h2>Judul blog</h2></a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea officia voluptates quia exercitationem!</p>
+            <a href="/read" class="read">READ MORE</a>
         </div>
-        <p>12 Febuari 2026</p>
-        <h2>Judul blog</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea officia voluptates quia exercitationem!</p>
-    </div>
-    <div class="blog">
         <div class="log1">
-            <img src="{{ asset('image/photo.6.jfif') }}" alt="">
-            <p>category</p>
+            <form action="/form">
+                <a href="">
+                <div class="log2">
+                    <a href=""><img src="{{ asset('image/photo.6.jfif') }}" alt=""></a>
+                    <button>category</button>
+                </div>
+            </form>
+            <p class="p">12 Febuari 2026</p>
+            <a href=""><h2>Judul blog</h2></a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea officia voluptates quia exercitationem!</p>
+            <a href="" class="read">READ MORE</a>
+            </a>
         </div>
-        <p>12 Febuari 2026</p>
-        <h2>Judul blog</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea officia voluptates quia exercitationem!</p>
     </div>
+    <div class="footer">
+            <button>LOAD MORE BLOG</button>
+        </div>
 </x-page>
